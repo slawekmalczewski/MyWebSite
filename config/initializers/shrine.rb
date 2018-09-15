e0000b40a56483a851ff9265b5029c46c8ef339e
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2018-09-15 19:21:49 +0100

    test

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -5,7 +5,7 @@ s3_options = {
   access_key_id:  ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
   region: ENV['AWS_S3_REGION'],
-  bucket: bucket: ENV['S3_BUCKET_NAME'],
+  bucket: ENV['S3_BUCKET_NAME'],
 }
 
 Shrine.storages = {
