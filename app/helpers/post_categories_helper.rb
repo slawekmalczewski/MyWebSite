ac967dcea3613fa978a0567008583f64eb0df56b
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2019-09-28 21:59:20 +0100

    Post Category solution 1

--- app/helpers/post_categories_helper.rb
+++ app/helpers/post_categories_helper.rb
@@ -1,7 +1,7 @@
 module PostCategoriesHelper
 
   def number_of_post_with_category(cat)
-    Post.where('post_category_id == ?',cat).count
+    Post.all.where('post_category_id == ?',cat).count
   end
 
 end
