d8700505fd61e9cc3fd5e9f4cdb3b1f812f1c2e5
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2020-01-02 23:25:22 +0000

    Comment functionality added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,15 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(required_parameters)
     redirect_to post_path(@post)
+    flash[:alert] = "Comment sucessfully added"
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+    flash[:alert] = "Comment sucessfully deleted"
   end
 
   private
