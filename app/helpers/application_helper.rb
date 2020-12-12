9dfec8e6ef4b56637d68e475ad1ebc0ddde88fae
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2020-12-12 00:33:37 +0000

    menu redesign

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,23 +2,23 @@ module ApplicationHelper
 
   def login_helper
     if current_user.is_a?(GuestUser)
-      "<li class = 'd-flex nav-link nav-link-auth align-items-baseline'>".html_safe +
+      "<li class='menu-link'>".html_safe +
       (link_to "Register", new_user_registration_path) +
       "</li>".html_safe +
-      "<li class = 'd-flex nav-link nav-link-auth align-items-baseline'>".html_safe + (link_to "Login", new_user_session_path) +
+      "<li class='menu-link'>".html_safe + (link_to "Login", new_user_session_path) +
       "</li>".html_safe
     elsif is_admin?
-      "<li class = 'd-flex nav-link nav-link-auth align-items-baseline'>".html_safe +
+      "<li class='menu-link'>".html_safe +
       (link_to "Admin Panel", controller: 'administrators', action: 'index') +
       "</li>".html_safe +
-      "<li class = 'd-flex nav-link nav-link-auth align-items-baseline'>".html_safe +
+      "<li class='menu-link'>".html_safe +
       (link_to "Logout", destroy_user_session_path, method: :delete) +
       "</li>".html_safe
     elsif is_registered_user?
-      "<li class = 'd-flex nav-link nav-link-auth align-items-baseline'>".html_safe +
+      "<li class='menu-link'>".html_safe +
       (link_to "User Panel", controller: 'administrators', action: 'user_index') +
       "</li>".html_safe +
-      "<li class = 'd-flex nav-link nav-link-auth align-items-baseline'>".html_safe +
+      "<li class='menu-link'>".html_safe +
       (link_to "Logout", destroy_user_session_path, method: :delete) +
       "</li>".html_safe
     end
