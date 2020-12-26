6c1a7f0301c0a1bc14d81f90623de822e1c1f677
Slawomir Malczewski <slawekmalczewski@hotmail.co.uk>
2020-12-26 22:08:06 +0000

    update

--- app/controllers/photo_galleries_controller.rb
+++ app/controllers/photo_galleries_controller.rb
@@ -6,7 +6,7 @@ class PhotoGalleriesController < ApplicationController
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :delete, :confirm_deletion]}, site_admin: :all
 
   def index
-    @gallery = PhotoGallery.where(:galleryVisibility => true).paginate(:page => params[:page], :per_page => 6).order('photo_galleries.galleryPosition')
+    @gallery = PhotoGallery.where(:galleryVisibility => true).order('galleryPosition asc').paginate(:page => params[:page], :per_page => 6)
     @numberOfImages = PhotoGallery.count
   end
 
